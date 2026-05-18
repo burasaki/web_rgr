3df913ecd8e7916b1eb02b2ff0ee3107dcc8e958
burasakid <burasakid@gmail.com>
2026-05-18 23:31:08 +0700

    fix asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,28 +1,21 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from webinar_project.consumers import ChatConsumer
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Инициализируем стандартное HTTP приложение Django
 django_asgi_app = get_asgi_application()
 
+
+from webinar_project.consumers import ChatConsumer
+
 application = ProtocolTypeRouter({
-    # Обычные HTTP запросы идут в стандартный Django
+    # Обычные HTTP-запросы
     "http": django_asgi_app,
     
-    # WebSocket запросы маршрутизируются в наш ChatConsumer
+    # WebSocket-соединения
     "websocket": URLRouter([
         path("ws/chat/", ChatConsumer.as_asgi()),
     ]),
